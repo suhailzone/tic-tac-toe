151feef4fbfe461a73202d593cb2c019e238a191
Sohail Shaikh <sohailshaikh07899@gmail.com>
2020-07-14 20:01:43 +0530

    Updates

---
+++ precache-manifest.89d1a8d00fe3f56bd36177660999772e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba961323c71848416757613813ad9d2",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0de48ca47587794f7273",
+    "url": "/tic-tac-toe/static/css/2.23a9c941.chunk.css"
+  },
+  {
+    "revision": "88e3a25eeb602c49fe0c",
+    "url": "/tic-tac-toe/static/css/main.cb8f92e7.chunk.css"
+  },
+  {
+    "revision": "0de48ca47587794f7273",
+    "url": "/tic-tac-toe/static/js/2.92b9ec1e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.92b9ec1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e3a25eeb602c49fe0c",
+    "url": "/tic-tac-toe/static/js/main.77fd2e7e.chunk.js"
+  },
+  {
+    "revision": "3c4dd662dd4ea0225d13",
+    "url": "/tic-tac-toe/static/js/runtime-main.0353b389.js"
+  }
+]);
